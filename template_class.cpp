9511652849115351644b39477a2a20d9b67dc7e4
Mansoor Khan <thisismansoorkhan@gmail.com>
2023-06-14 01:09:14 +0530

    Modify gitignore, add template class example

--- template_class.cpp
+++ template_class.cpp
@@ -1,11 +1,31 @@
 #include<iostream>
 using namespace std;
-
+template<class Ttype>
+class tmp_class
+{
+    Ttype a, b;
+public:
+    void getdata()
+    {
+        cout<<"Enter two values: ";
+        cin>>a>>b;
+    }
+    void sum();
+};
+template<class Ttype>
+void tmp_class<Ttype>::sum()
+{
+    cout<<a+b<<endl;
+}
 
 int main()
 {
-    cout<<"Integer addition: "<<add(3,5)<<endl;
-    cout<<"Double addition: "<<add(3.3,5.2);
+    tmp_class <int>s1{};
+    tmp_class <float>s2{};
+    s1.getdata();
+    s1.sum();
+    s2.getdata();
+    s2.sum();
 
     return 0;
 }
